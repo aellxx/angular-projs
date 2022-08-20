b5e48ae04b6f9c41ea5577fd7f1a7433fda30bcb
Ael <bluebayband@gmail.com>
2022-08-20 14:54:59 -0600

    add css and states in ts

--- teamApp/src/app/app.component.ts
+++ teamApp/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamApp';
+  // states
+  newMember = "";
+  memberArray: string[] = [];
+
+  // functions to manipulate state
+  addNewMember = () => {
+    this.memberArray.push(this.newMember);
+  }
 }
