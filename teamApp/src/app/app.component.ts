97ef4a2169d85db34fa1e01cbfb2eadac8e11c1b
Ael <bluebayband@gmail.com>
2022-08-21 14:16:52 -0400

    add more events

--- teamApp/src/app/app.component.ts
+++ teamApp/src/app/app.component.ts
@@ -9,9 +9,46 @@ export class AppComponent {
   // states
   newMember = "";
   memberArray: string[] = [];
+  errorMsg = "";
+  numberOfTeams: number | "" = "";
 
-  // functions to manipulate state
+  /* functions to manipulate state */
+  
+  // click event: adds new member to array of members
   addNewMember = () => {
-    this.memberArray.push(this.newMember);
+    if (!this.newMember) {
+      this.errorMsg = "Please add a name";
+    } else {
+      // add new member to array
+      this.memberArray.push(this.newMember);
+      // clear input and error error message
+      this.newMember = this.errorMsg = "";
+    }
   }
+
+  // input event: changes state of newMember whenever textbox is changed
+  onInputName = (member: string) => {
+    this.newMember = member; 
+    // console.log(this.newMember);
+  }
+
+  onEnterNumberOfTeams = (numberOfTeams: string) => {
+    this.numberOfTeams = Number(numberOfTeams);
+  }
+
+  generateTeams = () => {
+    if (this.numberOfTeams === 0) {
+      this.errorMsg = `You can't generate 0 teams with ${this.memberArray.length} people. Try again.`;
+    } else {
+      // generate random teams
+      for (let i=0; i < this.memberArray.length; ++i){
+        
+      }
+
+      // clear input field and error msg
+      this.numberOfTeams = 0;
+      this.errorMsg = "";
+    }
+  }
+  
 }
