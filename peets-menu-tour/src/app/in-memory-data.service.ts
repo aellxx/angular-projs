41ce9ba2fc511f7af55f0c801c4aae969fd14bd1
Ael <bluebayband@gmail.com>
2022-08-23 11:15:01 -0400

    get menus with http client

--- peets-menu-tour/src/app/in-memory-data.service.ts
+++ peets-menu-tour/src/app/in-memory-data.service.ts
@@ -8,7 +8,7 @@ import { Menu } from './menu';
 export class InMemoryDataService implements InMemoryDbService{
   // create new database with InMemoryDbService
   createDb() {
-    const menus = [
+    const menus: Menu[] = [
       {id: "americano", name: 'Americano', price: 3.35, description: "An Americano is two ingredients: espresso and hot water. When rich, hand-pulled Espresso Forte is poured into hot water, you retain the perfect crema in a pleasantly sippable cup with notes of malty hazelnut. The Americano is said to have been invented in Italy during WWII, when American GIs craved a mug of coffee they could sip and savor, but the Italians only had espresso machines for brewing.", imageId: "https://peets-shop.imgix.net/products/caffe-americano.png?v=1597269387&auto=format,compress&w=1080"},
       {id: "cappuccino", name: 'Cappuccino', price: 3.85, description: "This coffee-forward classic has just two ingredients, but requires the utmost barista skill. We hand-pull a perfect espresso and marble it with creamy microfoam (thick steamed milk), stretching the espresso's caramel notes and providing a smooth texture.", imageId: "https://peets-shop.imgix.net/products/cappuccino.png?v=1597269390&auto=format,compress&w=1080"},
       {id: "caffe-latte", name: "Caffe Latte", price: 3.90, description: "The word \"latte\" means \"milk\" in Italian. In our Caffe Latte, milk rounds out espresso's assertive flavors in a soft, enjoyable drink. Rich, hand-pulled Espresso Forte meets smooth steamed milk, topped with a layer of silky foam.", imageId: "https://peets-shop.imgix.net/products/caffe-latte.png?v=1597269391&auto=format,compress&w=1080"},
@@ -17,6 +17,6 @@ export class InMemoryDataService implements InMemoryDbService{
       {id: "yuzu-citrus-black-tea-shaker", name: "Yuzu Citrus Black Tea Shaker", price: 3.85, description: "Summer Solstice black tea, hand-shaken with lemonade, refreshing yuzu puree, ice, and a slice of lemon.", imageId:"https://peets-shop.imgix.net/products/yuzu-citrus-black-tea-shaker.png?v=1597269396&auto=format,compress&w=1080"},
       {id: "tropical-berry-green-tea-shaker", name: "Tropical Berry Green Tea Shaker", price: 3.85, description: "Green Tea Tropical hand-shaken with lemonade, sweet strawberry puree, ice, and real strawberries."},
     ]; 
-    return menus;
+    return {menus};
   }
 }
--- peets-menu-tour/src/app/menu.service.ts
+++ peets-menu-tour/src/app/menu.service.ts
@@ -3,6 +3,7 @@ import { Menu } from './menu';
 import { MENUS } from './menus';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,24 @@ import { MessageService } from './message.service';
 
 export class MenuService {
 
+  // URL to web API
+  //    api: base -> where the HTTP request is made
+  //    heros: collectionName -> the database
+  private menusURL = 'api/menus';
+
   // send messageService as a private parameter
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService, 
+    private http: HttpClient
+  ) { }
 
   // Service array getter
   getMenus(): Observable<Menu[]> {
-    // get all the menus in the form of OBservables
-    const menus = of(MENUS);
+    // get menus from the internal server 
+    const menus = this.http.get<Menu[]>(this.menusURL);
+    console.log(menus);
     // signal: finish getting data
-    this.messageService.add("MenuService: fetched menu data");
+    this.logMessage("fetched menu data");
     // return Observable of an array of Menus
     return menus;
   } 
@@ -29,9 +39,14 @@ export class MenuService {
     const menu = MENUS.find(menuToFind => menuToFind.id === id)!;
     console.log(menu);
     // signal: finish getting menu
-    this.messageService.add(`MenuService: fetched menu data with menu id: ${id}`);
+    this.logMessage(`fetched menu data with menu id: ${id}`);
     // return Observable of type Menu
     return of(menu);
+  }
+
+  // refractor logging message 
+  logMessage(message: string): void {
+    this.messageService.add(`MenuService: ${message}`);
   }
 
 }
